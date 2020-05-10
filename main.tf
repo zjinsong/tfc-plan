3cfb5ad73f8a34877e66360b86e758c6aa57454e
zjinsong <65064518+zjinsong@users.noreply.github.com>
2020-05-10 12:06:41 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,41 @@
 provider "aws" {
-  version = "2.33.0"
+   version = "2.33.0"
+   region = "${var.aws_region}"
+ }
 
-  region = var.aws_region
-}
+   resource "aws_lambda_function" "example" {
+   function_name = "my-function-tf"
+   role= "arn:aws-cn:iam::326449571384:role/lambda-s3-execution-role"
+   # The bucket name as created earlier with "aws s3api create-bucket"
+   s3_bucket = "repo-lambda"
+   s3_key    = "/lambda-test.zip"
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = var.db_table_name
+   # "main" is the filename within the zip file (main.js) and "handler"
+   # is the name of the property under which the handler function was
+   # exported in that file.
+   handler = "lambda_function.lambda_handler"
+   runtime = "python2.7"
+ }
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
+ # IAM role which dictates what other AWS services the Lambda function
+ # may access.
+ resource "aws_iam_role" "lambda_exec" {
+   name = "lambda-s3-execution-role"
 
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
+   assume_role_policy = <<EOF
+ {
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Action": "sts:AssumeRole",
+       "Principal": {
+         "Service": "lambda.amazonaws.com"
+       },
+       "Effect": "Allow",
+       "Sid": ""
+     }
+   ]
+ }
+ EOF
 
-  tags = {
-    user_name = "jinsong"
-  }
-}
+ }
