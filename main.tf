daac9fe054cb95d0d5ee0c27bbf4283110f5d126
zjinsong <65064518+zjinsong@users.noreply.github.com>
2020-05-10 14:49:55 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,27 +36,3 @@ provider "aws" {
 }
 EOF
 }
-resource "aws_iam_policy" "policy" {
-  name        = "lambda-policy"
-  description = "A lambda policy"
-  policy = <<EOF
- {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_role_policy_attachment" "lambda-attach" {
-  role       = "${aws_iam_role.role.name}"
-  policy_arn = "${aws_iam_policy.policy.arn}"
-}
