5e5fa8db21c39de5760b7fa0b6974ae3ecc0428b
zjinsong <65064518+zjinsong@users.noreply.github.com>
2020-05-10 17:01:45 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 provider "aws" {
-  version = "2.33.0"
-  region = var.aws_region
+ region = var.aws_region
 }
 
-   resource "aws_lambda_function" "example" {
-   function_name = "my-function"
-   role = "${aws_iam_role.role.arn}"
+ resource "aws_lambda_function" "example" {
+   function_name = "ServerlessExample"
+
    # The bucket name as created earlier with "aws s3api create-bucket"
    s3_bucket = "repo-lambda"
    s3_key    = "example.zip"
@@ -13,26 +12,31 @@ provider "aws" {
    # "main" is the filename within the zip file (main.js) and "handler"
    # is the name of the property under which the handler function was
    # exported in that file.
-   handler = "lambda_function.lambda_handler"
-   runtime = "python2.7"
+   handler = "main.handler"
+   runtime = "nodejs12.x"
+
+   role = aws_iam_role.lambda_exec.arn
  }
+
  # IAM role which dictates what other AWS services the Lambda function
  # may access.
-  resource "aws_iam_role" "role" {
-  name = "lambda-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
+ resource "aws_iam_role" "lambda_exec" {
+   name = "serverless_example_lambda"
+
+   assume_role_policy = <<EOF
+ {
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Action": "sts:AssumeRole",
+       "Principal": {
+         "Service": "lambda.amazonaws.com"
+       },
+       "Effect": "Allow",
+       "Sid": ""
+     }
+   ]
+ }
+ EOF
+
+ }
